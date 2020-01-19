e344b71fb509a26f6c8840547b599065dabde7bc
Erik Zemčík <zemcik.erik@gmail.com>
2020-01-19 23:54:57 +0100

    RecipePreview

---
+++ CooKit/CooKit/Views/RecipePreview.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CooKit.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Skip)]
+    public partial class RecipePreview
+    {
+
+        public static readonly BindableProperty ImageProperty = BindableProperty.Create(
+            propertyName: nameof(Image),
+            returnType: typeof(ImageSource),
+            declaringType: typeof(ImageSource)
+        );
+
+        public ImageSource Image
+        {
+            get => (ImageSource) GetValue(ImageProperty);
+            set => SetValue(ImageProperty, value);
+        }
+
+        public static readonly BindableProperty NameProperty = BindableProperty.Create(
+            propertyName: nameof(Name),
+            returnType: typeof(string),
+            declaringType: typeof(string)
+        );
+
+        public string Name
+        {
+            get => (string) GetValue(NameProperty);
+            set => SetValue(NameProperty, value);
+        }
+
+        public RecipePreview()
+        {
+            InitializeComponent();
+        }
+    }
+}
