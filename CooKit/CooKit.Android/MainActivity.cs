45d7c464caea2cc04c5fe6a131b970bbc33aad5f
Erik Zemčík <zemcik.erik@gmail.com>
2020-02-13 09:15:20 +0100

    Recipe View Basic Design

--- CooKit/CooKit.Android/MainActivity.cs
+++ CooKit/CooKit.Android/MainActivity.cs
@@ -1,10 +1,6 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 using Xamarin.Forms;
 
@@ -19,11 +15,12 @@ namespace CooKit.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-
+            
             Forms.SetFlags("CollectionView_Experimental");
+            Forms.SetFlags("CarouselView_Experimental");
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Forms.Init(this, savedInstanceState);
 
             LoadApplication(new App());
         }
---
+++ CooKit/CooKit/Views/RecipeView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace CooKit.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RecipeView
+    {
+        public RecipeView()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public sealed class TemporaryRecipeStep
+    {
+        public string Value { get; set; }
+    }
+}
