8ffdbbaba7aff8bce69d8d15e1c8e931864e19c0
Erik Zemčík <zemcik.erik@gmail.com>
2020-02-13 21:21:41 +0100

    Small project cleanup

--- CooKit/CooKit/Views/MainPage.xaml.cs
+++ CooKit/CooKit/Views/MainPage.xaml.cs
@@ -1,13 +1,15 @@
-﻿using Xamarin.Forms.Xaml;
+﻿using System;
+using Xamarin.Forms.Xaml;
 
 namespace CooKit.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage
     {
-        public MainPage()
-        {
+        public MainPage() => 
             InitializeComponent();
-        }
+
+        private void OnRecipeTapped(object sender, EventArgs e) =>
+            throw new NotImplementedException();
     }
 }
