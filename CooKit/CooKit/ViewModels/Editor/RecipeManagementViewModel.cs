c76dd89497befea8ebe19f1b7110a580ec2a19ac
Erik Zemčík <zemcik.erik@gmail.com>
2020-03-14 12:40:14 +0100

    Created shared implementation for management view models

--- CooKit/CooKit/ViewModels/Editor/RecipeManagementViewModel.cs
+++ CooKit/CooKit/ViewModels/Editor/RecipeManagementViewModel.cs
@@ -1,69 +1,11 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Windows.Input;
-using CooKit.Models;
-using CooKit.Services;
+﻿using CooKit.Models;
 using CooKit.Views.Editor;
-using Xamarin.Forms;
 
 namespace CooKit.ViewModels.Editor
 {
-    public sealed class RecipeManagementViewModel : BaseViewModel, IDisposable
+    public sealed class RecipeManagementViewModel : SharedManagementViewModel<IRecipe, IRecipeBuilder>
     {
-        public ObservableCollection<IRecipe> Recipes
-        {
-            get => _recipes;
-            private set => HandlePropertyChange(ref _recipes, value);
-        }
-        private ObservableCollection<IRecipe> _recipes;
-
-        public IRecipe SelectedRecipe
-        {
-            get => _selectedRecipe;
-            set => HandlePropertyChange(ref _selectedRecipe, value);
-        }
-        private IRecipe _selectedRecipe;
-
-        public ICommand AddCommand { get; }
-        public ICommand RemoveCommand { get; }
-
-        private readonly IRecipeStore _recipeStore;
-
-        public RecipeManagementViewModel()
-        {
-            _recipeStore = App.GetRecipeStore();
-            _recipes = new ObservableCollection<IRecipe>(_recipeStore.LoadedObjects);
-            _selectedRecipe = null;
-
-            AddCommand = new Command(HandleAdd);
-            RemoveCommand = new Command(HandleRemove);
-
-            _recipeStore.PropertyChanged += HandleStoreContentChange;
-        }
-
-        private static async void HandleAdd() =>
-            await Shell.Current.Navigation.PushAsync(new RecipeDesignerView());
-
-        private async void HandleRemove()
-        {
-            if (SelectedRecipe is null)
-                return;
-
-            await _recipeStore.RemoveAsync(SelectedRecipe.Id);
-            SelectedRecipe = null;
-        }
-
-        private void HandleStoreContentChange(object sender, PropertyChangedEventArgs e)
-        {
-            if (sender != _recipeStore)
-                return;
-
-            SelectedRecipe = null;
-            Recipes = new ObservableCollection<IRecipe>(_recipeStore.LoadedObjects);
-        }
-
-        public void Dispose() =>
-            _recipeStore.PropertyChanged -= HandleStoreContentChange;
+        public RecipeManagementViewModel() 
+            : base(App.GetRecipeStore(), typeof(RecipeDesignerView)) { }
     }
 }
---
+++ CooKit/CooKit/ViewModels/Editor/SharedManagementViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+using CooKit.Models;
+using CooKit.Services;
+using Xamarin.Forms;
+
+namespace CooKit.ViewModels.Editor
+{
+    public class SharedManagementViewModel<TStorable, TStorableBuilder> 
+        : BaseViewModel, IDisposable where TStorable : IStorable
+    {
+        public ObservableCollection<TStorable> StoredObjects
+        {
+            get => _storedObjects;
+            set => HandlePropertyChange(ref _storedObjects, value);
+        }
+        private ObservableCollection<TStorable> _storedObjects;
+
+        public TStorable SelectedObject
+        {
+            get => _selectedObject;
+            set => HandlePropertyChange(ref _selectedObject, value);
+        }
+        private TStorable _selectedObject;
+
+        public ICommand AddCommand { get; }
+        public ICommand RemoveCommand { get; }
+
+        private readonly IStoreBase<TStorable, TStorableBuilder> _store;
+        private readonly Type _designerPageType;
+
+        public SharedManagementViewModel(IStoreBase<TStorable, TStorableBuilder> store, Type designerPageType)
+        {
+            _store = store;
+            _designerPageType = designerPageType;
+
+            _selectedObject = default;
+            _storedObjects = new ObservableCollection<TStorable>(store.LoadedObjects);
+            
+            AddCommand = new Command(HandleAdd);
+            RemoveCommand = new Command(HandleRemove);
+
+            _store.PropertyChanged += HandleStoreContentChange;
+        }
+
+        private async void HandleAdd()
+        {
+            var page = (Page) Activator.CreateInstance(_designerPageType);
+            await Shell.Current.Navigation.PushAsync(page);
+        }
+
+        private async void HandleRemove()
+        {
+            if (SelectedObject is null)
+                return;
+
+            await _store.RemoveAsync(SelectedObject.Id);
+            SelectedObject = default;
+        }
+
+        private void HandleStoreContentChange(object sender, PropertyChangedEventArgs e)
+        {
+            if (sender != _store)
+                return;
+
+            SelectedObject = default;
+            StoredObjects = new ObservableCollection<TStorable>(_store.LoadedObjects);
+        }
+
+        public void Dispose() =>
+            _store.PropertyChanged -= HandleStoreContentChange;
+    }
+}
