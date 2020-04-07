3d4e514f17ae1da065121dcb73e044b1905c1f6b
Erik Zemčík <zemcik.erik@gmail.com>
2020-04-07 19:15:47 +0200

    Project recreation

--- src/CooKit/CooKit.Android/MainActivity.cs
+++ src/CooKit/CooKit.Android/MainActivity.cs
@@ -2,12 +2,10 @@
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
-using Xamarin.Essentials;
-using Xamarin.Forms;
 
-namespace CooKit.Mobile.Droid
+namespace CooKit.Droid
 {
-    [Activity(Label = "CooKit.Mobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "CooKit", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -17,14 +15,14 @@ namespace CooKit.Mobile.Droid
 
             base.OnCreate(savedInstanceState);
 
-            Platform.Init(this, savedInstanceState);
-            Forms.Init(this, savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            Xamarin.Forms.Forms.Init(this, savedInstanceState);
 
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
         {
-            Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
--- src/CooKit/CooKit.Android/Properties/AssemblyInfo.cs
+++ src/CooKit/CooKit.Android/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CooKit.Mobile.Android")]
+[assembly: AssemblyTitle("CooKit.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CooKit.Mobile.Android")]
+[assembly: AssemblyProduct("CooKit.Android")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
