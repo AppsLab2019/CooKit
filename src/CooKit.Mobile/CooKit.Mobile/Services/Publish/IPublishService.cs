bbfd12c71c6fe5913b3b7bf268b660263de24dc9
Erik Zemčík <zemcik.erik@gmail.com>
2020-08-24 16:40:22 +0200

    Added missing files

---
+++ src/CooKit.Mobile/CooKit.Mobile/Services/Publish/IPublishService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using CooKit.Mobile.Models;
+
+namespace CooKit.Mobile.Services.Publish
+{
+    public interface IPublishService
+    {
+        Task PublishRecipeAsync(Recipe recipe, IProgress<string> progress);
+    }
+}
---
+++ src/CooKit.Mobile/CooKit.Mobile/Services/Publish/MockPublishService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using CooKit.Mobile.Models;
+
+namespace CooKit.Mobile.Services.Publish
+{
+    public class MockPublishService : IPublishService
+    {
+        public async Task PublishRecipeAsync(Recipe recipe, IProgress<string> progress)
+        {
+            if (recipe == null)
+                throw new ArgumentNullException(nameof(recipe));
+
+            for (var i = 0; i < 3; i++)
+            {
+                progress?.Report($"Uploading image {i + 1}.");
+                await Task.Delay(2000);
+            }
+
+            progress?.Report("Publishing recipe!");
+            await Task.Delay(1000);
+        }
+    }
+}
